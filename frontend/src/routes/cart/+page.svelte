14532b5fff8d6e56b79f19c361991ecba9cf4175
andrewzw <yapzhewei2002@gmail.com>
2023-06-01 21:15:17 +1000

    useless cart

--- frontend/src/routes/cart/+page.svelte
+++ frontend/src/routes/cart/+page.svelte
@@ -1,10 +1,69 @@
+<script>
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+
+	let cart = [];
+	let customerId = null;
+
+	onMount(async () => {
+		// Retrieve the customerId from session storage
+		customerId = sessionStorage.getItem('customerId');
+		if (!customerId) {
+			alert('Please log in to view your cart');
+			goto('/login');
+			return;
+		}
+
+		const res = await fetch('http://localhost:4000/getCart', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ customerId })
+		});
+		const data = await res.json();
+		cart = data.orderItems;
+	});
+
+	async function removeFromCart(orderItemId) {
+		const res = await fetch('http://localhost:4000/removeFromCart', {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ customerId, orderItemId })
+		});
+		const data = await res.json();
+		if (data.success) {
+			cart = cart.filter((item) => item._id !== orderItemId);
+		} else {
+			alert('Failed to remove product from cart');
+		}
+	}
+</script>
+
 <svelte:head>
 	<title>Shopping Cart</title>
 	<meta name="description" content="Shopping Cart" />
 </svelte:head>
 
 <div class="text-column">
-	<h1>Shopping Cart</h1>
+	<h1 class="mt-4">Cart</h1>
 
-	<!-- Shopping cart -->
+	{#if cart.length === 0}
+		<p>Your cart is empty</p>
+	{:else}
+		<ul class="list-group">
+			{#each cart as item}
+				<li class="list-group-item">
+					<h3 class="mb-1">{item.product.name}</h3>
+					<p class="mb-1"><strong>Quantity:</strong> {item.quantity}</p>
+					<p class="mb-1"><strong>Price:</strong> ${item.price}</p>
+					<button class="btn btn-danger mt-2" on:click={() => removeFromCart(item._id)}
+						>Remove from Cart</button
+					>
+				</li>
+			{/each}
+		</ul>
+	{/if}
 </div>
